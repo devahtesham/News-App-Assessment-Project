d170d8262b45d2a8bb556a3778adcfa37fba3d26
Ahtesham Akram <ahte02254@gmail.com>
2023-12-10 21:39:36 +0500

    lazy load components for better optimization

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { Route, Routes } from 'react-router-dom'
-import { Home,DetailPage } from '../pages'
+import Loader from '../components/UI/Loader';
+
+// importing components dynamically for optimization perspective
+const Home = React.lazy(() => import("../pages/Home"));
+const DetailPage = React.lazy(() => import("../pages/DetailPage"));
 
 const Router = () => {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/:id' element={<DetailPage />} />
+      <Route path='/' element={
+        <Suspense fallback={<Loader />}>
+          <Home />
+        </Suspense>
+      } />
+      <Route path='/:id' element={
+        <Suspense fallback={<Loader />}>
+          <DetailPage />
+        </Suspense>
+      } />
     </Routes>
   )
 }
